961d898aad29da9665a32a4891d723e3b8593d34
VANSHTRIVEDI <vanshtrivedi12345@gmail.com>
2024-01-27 23:40:54 +0530

    LeetCode Questions

--- Easy/recursion/509.Fibonacci_Number.java
+++ Easy/recursion/509.Fibonacci_Number.java
@@ -6,12 +6,12 @@ class Solution {
     public int fib(int n) {
 
         if (n == 1) {
-            return 1;
+            return 1; // base condition
         }
         if (n == 0) {
-            return 0;
+            return 0; // base condition
         }
-        int sum = fib(n - 1) + fib(n - 2);
+        int sum = fib(n - 1) + fib(n - 2); // recursive funtion
         return sum;
 
     }
