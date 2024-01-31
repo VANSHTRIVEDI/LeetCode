3e8f576e6f40bb6961f97ce57e8bc634c61a442a
VANSHTRIVEDI <vanshtrivedi12345@gmail.com>
2024-01-31 23:09:57 +0530

    LeetCode Questions

---
+++ Easy/Binary_Tree/222. Count_Complete_Tree_Nodes.java
@@ -0,0 +1,40 @@
+package Easy.Binary_Tree;
+
+import java.util.*;
+
+//   Definition for a binary tree node.
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Solution {
+    public static void lrc(TreeNode root, int arr[]) {
+        if (root == null) {
+            return;
+        }
+        arr[0] = arr[0] + 1;
+        lrc(root.left, arr);
+        lrc(root.right, arr);
+    }
+
+    public int countNodes(TreeNode root) {
+        int arr[] = new int[1];
+        lrc(root, arr);
+        return arr[0];
+    }
+}
