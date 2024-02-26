c7e2c1a6d403190f1cba7c136907dfa1788e44ae
VANSHTRIVEDI <vanshtrivedi12345@gmail.com>
2024-02-26 23:18:45 +0530

    LeetCode Questions

--- BINARY_TREE/MEDIUM/Kth_AncestorTree_M.java
+++ BINARY_TREE/MEDIUM/Kth_AncestorTree_M.java
@@ -37,3 +37,51 @@ class Kth_AncestorTree_M {
 
     }
 }
+
+class Kth_AncestorTree_M2 {
+    public int kthAncestor(Node root, int k, int node) {
+        int arr[] = new int[1];
+        arr[0] = k;
+        Node temp = solve(root, node, arr);
+        // System.out.println("array"arr);
+        if (temp == null || temp.data == node) {
+            return -1;
+        } else {
+            return temp.data;
+        }
+
+    }
+
+    public static Node solve(Node root, int node, int arr[]) {
+        if (root == null) {
+            return null;
+        }
+        if (root.data == node) {
+            return root;
+        }
+
+        Node left = solve(root.left, node, arr);
+        Node right = solve(root.right, node, arr);
+
+        if (left != null && right == null) {
+            arr[0] = arr[0] - 1;
+            if (arr[0] <= 0) {
+                arr[0] = Integer.MAX_VALUE;
+                return root;
+            }
+
+            return left;
+        }
+        if (right != null && left == null) {
+            arr[0] = arr[0] - 1;
+            if (arr[0] <= 0) {
+                arr[0] = Integer.MAX_VALUE;
+                return root;
+            }
+
+            return right;
+        }
+        return null;
+
+    }
+}
