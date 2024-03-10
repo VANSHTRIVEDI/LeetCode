ac6b3af14798b748616dd2f1af21bb8fcb1b351f
VANSHTRIVEDI <vanshtrivedi12345@gmail.com>
2024-03-10 20:05:12 +0530

    LeetCode Questions

--- BInary_Search_Tree/MEDIUM/Predecessor_and_Successor.java
+++ BInary_Search_Tree/MEDIUM/Predecessor_and_Successor.java
@@ -4,71 +4,73 @@ import java.util.ArrayList;
 import java.util.List;
 
 //https://www.geeksforgeeks.org/problems/predecessor-and-successor/1
-class TreeNode {
-    int data;
-    TreeNode left;
-    TreeNode right;
+//using this treenode and PredandSucc is only accessable to myfile class
+class MyFile {
+    private static class TreeNode {
+        int data;
+        TreeNode left;
+        TreeNode right;
 
-    TreeNode() {
-        this.data = 0;
-        this.left = null;
-        this.right = null;
-    }
-
-    TreeNode(int data) {
-        this.data = data;
-        this.left = null;
-        this.right = null;
-    }
-
-    TreeNode(int data, TreeNode left, TreeNode right) {
-        this.data = data;
-        this.left = left;
-        this.right = right;
-    }
-};
-
-class PredandSucc {
-
-    // most important best way
-    public static List<Integer> predecessorSuccessor(TreeNode root, int key) {
-        TreeNode temp = root;
-        int pred = -1;
-        int succ = -1;
-
-        while (temp != null) {
-            if ((int) temp.data > key) {
-                succ = (int) temp.data;
-                temp = temp.left;
-
-            } else if (temp.data < key) {
-                pred = (int) temp.data;
-                temp = temp.right;
-
-            } else {
-
-                if (temp.left != null) {
-                    TreeNode preee = temp.left;
-                    while (preee.right != null) {
-                        preee = preee.right;
-                    }
-                    pred = (int) preee.data;
-                }
-
-                if (temp.right != null) {
-                    TreeNode succc = temp.right;
-                    while (succc.left != null) {
-                        succc = succc.left;
-                    }
-                    succ = (int) succc.data;
-                }
-                break;
-            }
+        TreeNode() {
+            this.data = 0;
+            this.left = null;
+            this.right = null;
         }
-        List<Integer> arr = new ArrayList<>();
-        arr.add(pred);
-        arr.add(succ);
-        return arr;
 
+        TreeNode(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+
+        TreeNode(int data, TreeNode left, TreeNode right) {
+            this.data = data;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    private static class PredandSucc {
+
+        // most important best way
+        public static List<Integer> predecessorSuccessor(TreeNode root, int key) {
+            TreeNode temp = root;
+            int pred = -1;
+            int succ = -1;
+
+            while (temp != null) {
+                if (temp.data > key) {
+                    succ = temp.data;
+                    temp = temp.left;
+
+                } else if (temp.data < key) {
+                    pred = temp.data;
+                    temp = temp.right;
+
+                } else {
+
+                    if (temp.left != null) {
+                        TreeNode preee = temp.left;
+                        while (preee.right != null) {
+                            preee = preee.right;
+                        }
+                        pred = preee.data;
+                    }
+
+                    if (temp.right != null) {
+                        TreeNode succc = temp.right;
+                        while (succc.left != null) {
+                            succc = succc.left;
+                        }
+                        succ = succc.data;
+                    }
+                    break;
+                }
+            }
+            List<Integer> arr = new ArrayList<>();
+            arr.add(pred);
+            arr.add(succ);
+            return arr;
+        }
     }
 }
