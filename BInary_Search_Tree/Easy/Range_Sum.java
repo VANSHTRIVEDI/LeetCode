d449ca5d17263f302dd3a34f5b14b13e24b2f794
VANSHTRIVEDI <vanshtrivedi12345@gmail.com>
2024-03-11 17:21:50 +0530

    LeetCode Questions

--- BInary_Search_Tree/Easy/Range_Sum.java
+++ BInary_Search_Tree/Easy/Range_Sum.java
@@ -17,3 +17,22 @@ class Range {
 
     }
 }
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
