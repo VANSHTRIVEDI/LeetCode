1cb301dec5203a139908fa0b217c7d94d979dc8a
VANSHTRIVEDI <vanshtrivedi12345@gmail.com>
2024-03-28 22:33:41 +0530

    LeetCode Questions

---
+++ Medium/Miscellaneous/Length_of_longest.java
@@ -0,0 +1,23 @@
+package Medium.Miscellaneous;
+
+import java.util.*;
+
+//https://leetcode.com/problems/length-of-longest-subarray-with-at-most-k-frequency/description/?envType=daily-question&envId=2024-03-28
+class Solution {
+    public int maxSubarrayLength(int[] nums, int k) {
+        int ans = 0;
+        HashMap<Integer, Integer> mp = new HashMap<>();
+        int l = 0;
+
+        for (int r = 0; r < nums.length; r++) {
+            mp.put(nums[r], mp.getOrDefault(nums[r], 0) + 1);
+            while (mp.get(nums[r]) > k) {
+                mp.put(nums[l], mp.get(nums[l]) - 1);
+                l++;
+            }
+            ans = Math.max(ans, r - l + 1);
+        }
+
+        return ans;
+    }
+}
