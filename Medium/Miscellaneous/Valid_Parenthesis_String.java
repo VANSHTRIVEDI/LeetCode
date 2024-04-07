a187b2621794acb012a69706bb5a6577895915b7
VANSHTRIVEDI <vanshtrivedi12345@gmail.com>
2024-04-07 23:37:58 +0530

    LeetCode Questions

---
+++ Medium/Miscellaneous/Valid_Parenthesis_String.java
@@ -0,0 +1,27 @@
+package Medium.Miscellaneous;
+
+//https://leetcode.com/problems/valid-parenthesis-string/?envType=daily-question&envId=2024-04-07
+public class Valid_Parenthesis_String {
+    public boolean checkValidString(String s) {
+        int leftMin = 0, leftMax = 0;
+
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                leftMin++;
+                leftMax++;
+            } else if (c == ')') {
+                leftMin--;
+                leftMax--;
+            } else {
+                leftMin--;
+                leftMax++;
+            }
+            if (leftMax < 0)
+                return false;
+            if (leftMin < 0)
+                leftMin = 0;
+        }
+
+        return leftMin == 0;
+    }
+}
