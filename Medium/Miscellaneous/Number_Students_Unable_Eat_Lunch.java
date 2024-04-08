b2193063b5f1a18e6bc09dd9a2fa3d39cb948d63
VANSHTRIVEDI <vanshtrivedi12345@gmail.com>
2024-04-08 23:20:43 +0530

    LeetCode Questions

---
+++ Medium/Miscellaneous/Number_Students_Unable_Eat_Lunch.java
@@ -0,0 +1,20 @@
+package Medium.Miscellaneous;
+
+public class Number_Students_Unable_Eat_Lunch {
+    public int countStudents(int[] students, int[] sandwiches) {
+        int[] counts = new int[2];
+        for (int student : students)
+            counts[student]++;
+
+        int remaining = sandwiches.length;
+        for (int sandwich : sandwiches) {
+            if (counts[sandwich] == 0)
+                break;
+            if (remaining-- == 0)
+                break;
+            counts[sandwich]--;
+        }
+
+        return remaining;
+    }
+}
