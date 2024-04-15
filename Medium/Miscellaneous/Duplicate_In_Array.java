6c0cd619f3b58c0f91a8c6ae13cfd0a0a7c5c699
VANSHTRIVEDI <vanshtrivedi12345@gmail.com>
2024-04-15 23:24:16 +0530

    LeetCode Questions

---
+++ Medium/Miscellaneous/Duplicate_In_Array.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Duplicate_In_Array {
+    public static int findDuplicate(ArrayList<Integer> arr) {
+
+        HashSet<Integer> hs = new HashSet<>();
+
+        for (int num : arr)
+
+        {
+
+            if (hs.contains(num))
+
+            {
+
+                return num;
+
+            }
+
+            else {
+
+                hs.add(num);
+            }
+
+        }
+
+        return -1;
+
+    }
+}
