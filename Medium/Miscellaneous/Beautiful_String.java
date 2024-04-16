930f21506bec970ef42cdec5fbbcc411c6cc7bed
VANSHTRIVEDI <vanshtrivedi12345@gmail.com>
2024-04-16 22:13:34 +0530

    LeetCode Questions

---
+++ Medium/Miscellaneous/Beautiful_String.java
@@ -0,0 +1,35 @@
+public class Beautiful_String {
+    public static int calculateCount(String str, char c) {
+
+        int count = 0;
+
+        for (int i = 0; i < str.length(); i = i + 2) {
+
+            if (str.charAt(i) != c) {
+
+                count++;
+
+            }
+
+        }
+
+        for (int i = 1; i < str.length(); i = i + 2) {
+
+            if (str.charAt(i) == c) {
+
+                count++;
+
+            }
+
+        }
+
+        return count;
+
+    }
+
+    public static int makeBeautiful(String str) {
+
+        return Math.min(calculateCount(str, '0'), calculateCount(str, '1'));
+
+    }
+}
