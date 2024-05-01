f118c8bfa38c98255a63e5b7c9ead8c948f3a7c5
VANSHTRIVEDI <vanshtrivedi12345@gmail.com>
2024-05-02 04:26:09 +0530

    LeetCode Questions

---
+++ Medium/Miscellaneous/Reverse_Prefix_Word.java
@@ -0,0 +1,12 @@
+package Medium.Miscellaneous;
+
+public class Reverse_Prefix_Word {
+    public String reversePrefix(String word, char ch) {
+        int j = word.indexOf(ch);
+        if (j != -1) {
+            return new StringBuilder(word.substring(0, j + 1)).reverse().toString() +
+                    word.substring(j + 1);
+        }
+        return word;
+    }
+}
