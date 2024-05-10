75b75735f3ba689eecc5a98a73c6c4a1f51e8367
VANSHTRIVEDI <vanshtrivedi12345@gmail.com>
2024-05-10 17:24:38 +0530

    LeetCode Questions

---
+++ Medium/Miscellaneous/K-th_Smallest_Prime_Fraction.java
@@ -0,0 +1,43 @@
+package Medium.Miscellaneous;
+
+//https://leetcode.com/problems/k-th-smallest-prime-fraction/?envType=daily-question&envId=2024-05-10
+class Kth_Smallest_Prime_Fraction {
+    public int[] kthSmallestPrimeFraction(int[] arr, int k) {
+        int n = arr.length;
+        double low = arr[0] / (double) arr[n - 1];
+        double high = 1;
+        while (low <= high) {
+            double mid = (low + high) / 2;
+            int count[] = f(arr, mid); // {count, num, deno}
+            if (k < count[0]) {
+                high = mid;
+            } else if (k > count[0]) {
+                low = mid;
+            } else {
+                return new int[] { count[1], count[2] };
+            }
+        }
+        return null;
+    }
+
+    static int[] f(int arr[], double target) {
+        int count = 0;
+        int i = 0;
+        int n = arr.length;
+        int num = arr[0];
+        int deno = arr[n - 1];
+        for (int j = 1; j < n; j++) {
+
+            while (arr[i] <= arr[j] * target) {
+                i++;
+            }
+
+            count += i;
+            if (i > 0 && arr[i - 1] * deno > num * arr[j]) {
+                num = arr[i - 1];
+                deno = arr[j];
+            }
+        }
+        return new int[] { count, num, deno };
+    }
+}
