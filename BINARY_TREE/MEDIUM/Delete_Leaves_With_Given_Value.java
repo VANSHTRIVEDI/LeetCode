744931e00312af94f68d85f355a56a022c6743c3
VANSHTRIVEDI <vanshtrivedi12345@gmail.com>
2024-05-17 22:41:20 +0530

    LeetCode Questions

---
+++ BINARY_TREE/MEDIUM/Delete_Leaves_With_Given_Value.java
@@ -0,0 +1,21 @@
+package BINARY_TREE.MEDIUM;
+
+public class Delete_Leaves_With_Given_Value {
+    public TreeNode removeLeafNodes(TreeNode root, int target) {
+        if (root == null) {
+            return null;
+        }
+        if (root.left != null) {
+            root.left = removeLeafNodes(root.left, target);
+        }
+        if (root.right != null) {
+            root.right = removeLeafNodes(root.right, target);
+        }
+
+        if (root.val == target && root.left == null && root.right == null) {
+            return null;
+        } else {
+            return root;
+        }
+    }
+}
