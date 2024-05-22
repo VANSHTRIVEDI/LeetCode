33c91c1582a0ed1274cd8b40c40dba53e70b8ff2
VANSHTRIVEDI <vanshtrivedi12345@gmail.com>
2024-05-22 23:41:26 +0530

    LeetCode Questions

---
+++ Medium/recursion_backtrack/Palindrome_Partitioning.java
@@ -0,0 +1,85 @@
+package Medium.recursion_backtrack;
+
+import java.util.*;
+
+//best method
+public class Palindrome_Partitioning {
+    public List<List<String>> partition(String s) {
+        List<List<String>> result = new ArrayList<>();
+        backtrack(result, new ArrayList<>(), s, 0);
+        return result;
+    }
+
+    private void backtrack(List<List<String>> result, List<String> tempList, String s, int start) {
+        if (start == s.length()) {
+            result.add(new ArrayList<>(tempList));
+            return;
+        }
+
+        for (int end = start; end < s.length(); end++) {
+            if (isPalindrome(s, start, end)) {
+                tempList.add(s.substring(start, end + 1));
+                backtrack(result, tempList, s, end + 1);
+                tempList.remove(tempList.size() - 1);
+            }
+        }
+    }
+
+    private boolean isPalindrome(String s, int low, int high) {
+        while (low < high) {
+            if (s.charAt(low++) != s.charAt(high--)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+// my method
+
+class Solution {
+    static List<List<String>> f;
+
+    public List<List<String>> partition(String s) {
+        f = new ArrayList<>();
+        List<String> arr = new ArrayList<>();
+        recursion(s, Character.toString(s.charAt(0)), 1);
+        return f;
+    }
+
+    public static void recursion(String s, String r, int i) {
+        List<String> auu = new ArrayList<>();
+        if (i >= s.length()) {
+            String arr[] = r.split(" ");
+            for (int t = 0; t < arr.length; t++) {
+                if (Palindrome(arr[t]) == false) {
+                    return;
+                }
+                auu.add(arr[t]);
+            }
+
+            f.add(auu);
+            return;
+
+        }
+        recursion(s, r + Character.toString(s.charAt(i)), i + 1);
+
+        recursion(s, r + " " + Character.toString(s.charAt(i)), i + 1);
+
+    }
+
+    public static boolean Palindrome(String s) {
+        int n = s.length();
+        if (n == 1) {
+            return true;
+        } else {
+
+            for (int i = 0; i < n / 2; i++) {
+                if (s.charAt(i) != s.charAt(n - i - 1)) {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ Medium/recursion_backtrack/Subset.java
@@ -0,0 +1,138 @@
+package Medium.recursion_backtrack;
+
+// /https://leetcode.com/problems/subsets/?envType=daily-question&envId=2024-05-21
+import java.util.ArrayList;
+import java.util.List;
+
+public class Subset {
+    static List<List<Integer>> finall;
+
+    public List<List<Integer>> subsets(int[] nums) {
+
+        finall = new ArrayList<>();
+        List<Integer> arr = new ArrayList<>();
+        backtrack(nums, arr, 0);
+        return finall;
+    }
+
+    public static void backtrack(int[] nums, List<Integer> arr, int i) {
+        if (!finall.contains(arr))
+            finall.add(new ArrayList<>(arr));
+
+        if (i > nums.length - 1) {
+            return;
+        }
+
+        arr.add(nums[i]);
+        backtrack(nums, arr, i + 1);// takes
+        arr.remove(arr.size() - 1);
+        backtrack(nums, arr, i + 1);// not takes
+    }
+
+}
+
+// Iterative Approch
+// cascading style
+
+// class Solution {
+// static List<List<Integer>> finall;
+// public List<List<Integer>> subsets(int[] nums) {
+// List<List<Integer>> finall = new ArrayList<>();
+// finall.add(new ArrayList<>()); // Start with the empty subset
+
+// for (int num : nums) {
+// int size = finall.size();
+// for (int i = 0; i < size; i++) {
+// List<Integer> subset = new ArrayList<>(finall.get(i));
+// subset.add(num);
+// finall.add(subset);
+// }
+// }
+
+// return finall;
+// }
+// }
+
+// backtracking
+
+// class Solution {
+// static List<List<Integer>> finall;
+// public List<List<Integer>> subsets(int[] nums) {
+// finall=new ArrayList<>();
+// List<Integer> arr=new ArrayList<>();
+// backtrack(nums,arr,0);
+// return finall;
+// }
+// public static void backtrack(int[] nums,List<Integer> arr,int i)
+// {
+// if(!finall.contains(arr))
+// finall.add(new ArrayList<>(arr));
+
+// if(i>nums.length-1)
+// {
+// return;
+// }
+
+// arr.add(nums[i]);
+// backtrack(nums,arr,i+1);
+// arr.remove(arr.size()-1);
+// backtrack(nums,arr,i+1);
+// }
+// }
+
+// backtracking
+
+// public class Solution {
+// public List<List<Integer>> subsets(int[] nums) {
+// List<List<Integer>> finall = new ArrayList<>();
+// dfs(nums, 0, new ArrayList<>(), finall);
+// return finall;
+// }
+
+// private void dfs(int[] nums, int index, List<Integer> path,
+// List<List<Integer>> result) {
+// result.add(new ArrayList<>(path));
+
+// for (int i = index; i < nums.length; i++) {
+// path.add(nums[i]);
+// dfs(nums, i + 1, path, result);
+// path.remove(path.size() - 1); // Backtrack
+// }
+// }
+
+// public static void main(String[] args) {
+// Solution solution = new Solution();
+// int[] nums = {1, 2, 3};
+// List<List<Integer>> result = solution.subsets(nums);
+// System.out.println(result);
+// }
+// }
+
+// bits manipulation
+
+// public class Solution {
+// public List<List<Integer>> subsets(int[] nums) {
+// List<List<Integer>> finall = new ArrayList<>();
+// int n = nums.length;
+// int numOfSubsets = 1 << n; // 2^n subsets
+
+// for (int i = 0; i < numOfSubsets; i++) {
+// List<Integer> subset = new ArrayList<>();
+// for (int j = 0; j < n; j++) {
+// if ((i & (1 << j)) != 0) {
+// subset.add(nums[j]);
+// }
+// }
+// finall.add(subset);
+// }
+
+// return finall;
+// }
+
+// public static void main(String[] args) {
+// Solution solution = new Solution();
+// int[] nums = {1, 2, 3};
+// List<List<Integer>> result = solution.subsets(nums);
+// System.out.println(result);
+// }
+// }
