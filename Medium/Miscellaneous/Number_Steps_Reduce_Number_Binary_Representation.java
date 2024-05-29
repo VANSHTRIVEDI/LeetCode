afec81d65c5124584c0c9d8cf59d99ce5426fba6
VANSHTRIVEDI <vanshtrivedi12345@gmail.com>
2024-05-29 23:21:23 +0530

    Leetcode Question

---
+++ Medium/Miscellaneous/Number_Steps_Reduce_Number_Binary_Representation.java
@@ -0,0 +1,32 @@
+package Medium.Miscellaneous;
+
+public class Number_Steps_Reduce_Number_Binary_Representation {
+    public int numSteps(String s) {
+        int l = s.length() - 1;
+        int carry = 0;
+        int count = 0;
+
+        while (l > 0) {
+
+            if (Character.getNumericValue(s.charAt(l)) + carry == 0) {
+                carry = 0;
+                count++;
+
+            } else if (Character.getNumericValue(s.charAt(l)) + carry == 2) {
+                carry = 1;
+                count++;
+
+            } else {
+                carry = 1;
+                count += 2;
+            }
+            l--;
+        }
+
+        if (carry == 1) {
+            count++;
+        }
+
+        return count;
+    }
+}
