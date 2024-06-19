ba6a298bb63b23fdc23646941da2ad1c7f1bc6c5
VANSHTRIVEDI <vanshtrivedi12345@gmail.com>
2024-06-19 23:54:00 +0530

    Leetcode Questions

---
+++ Medium/Miscellaneous/Minimum_Number_Days_Make_m_Bouquets.java
@@ -0,0 +1,47 @@
+package Medium.Miscellaneous;
+
+//https://leetcode.com/problems/minimum-number-of-days-to-make-m-bouquets/?envType=daily-question&envId=2024-06-19
+public class Minimum_Number_Days_Make_m_Bouquets {
+    public int minDays(int[] bloomDay, int m, int k) {
+        if ((long) m * k > bloomDay.length) {
+            return -1;
+        }
+
+        int low = 1, high = (int) 1e9;
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+
+            if (isPossibleBouquets(bloomDay, m, k, mid)) {
+                high = mid;
+            } else {
+                low = mid + 1;
+            }
+        }
+        ;
+
+        return low;
+    }
+
+    private boolean isPossibleBouquets(int[] bloomDay, int m, int k, int day) {
+        int total = 0;
+
+        for (int i = 0; i < bloomDay.length; i++) {
+            int count = 0;
+            while (i < bloomDay.length && count < k && bloomDay[i] <= day) {
+                count++;
+                i++;
+            }
+
+            if (count == k) {
+                total++;
+                i--;
+            }
+
+            if (total >= m) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
