9953abcd8368a637b00505308c5758ec24330dab
VANSHTRIVEDI <vanshtrivedi12345@gmail.com>
2024-06-24 23:45:29 +0530

    Leetcode Questions

---
+++ Medium/Miscellaneous/Ceil_The_Floor.java
@@ -0,0 +1,50 @@
+package Medium.Miscellaneous;
+
+public class Ceil_The_Floor {
+    public static int[] getFloorAndCeil(int[] arr, int n, int x) {
+
+        // Wriute your code here.
+
+        int floor = -1, ceil = -1;
+
+        int left = 0, right = n - 1;
+
+        while (left <= right) {
+
+            int mid = left + ((right - left) / 2);
+
+            if (arr[mid] == x) {
+
+                floor = x;
+
+                ceil = x;
+
+                break;
+
+            } else if (arr[mid] > x) {
+
+                ceil = arr[mid];
+
+                right = mid - 1;
+
+            } else {
+
+                floor = arr[mid];
+
+                left = mid + 1;
+
+            }
+
+        }
+
+        int[] ans = new int[2];
+
+        ans[0] = floor;
+
+        ans[1] = ceil;
+
+        return ans;
+
+    }
+
+}
