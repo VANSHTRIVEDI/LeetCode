e09ae7bc4dda206dfeca906ab2f6fa9e10b0a133
VANSHTRIVEDI <vanshtrivedi12345@gmail.com>
2024-08-16 22:55:58 +0530

    Leetcode Question

--- Medium/recursion_backtrack/Subset.java
+++ Medium/recursion_backtrack/Subset.java
@@ -32,107 +32,107 @@ public class Subset {
 }
 
 // Iterative Approch
-// cascading style
 
-// class Solution {
-// static List<List<Integer>> finall;
-// public List<List<Integer>> subsets(int[] nums) {
-// List<List<Integer>> finall = new ArrayList<>();
-// finall.add(new ArrayList<>()); // Start with the empty subset
+class subset2 {
+    static List<List<Integer>> finall;
 
-// for (int num : nums) {
-// int size = finall.size();
-// for (int i = 0; i < size; i++) {
-// List<Integer> subset = new ArrayList<>(finall.get(i));
-// subset.add(num);
-// finall.add(subset);
-// }
-// }
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> finall = new ArrayList<>();
+        finall.add(new ArrayList<>()); // Start with the empty subset
 
-// return finall;
-// }
-// }
+        for (int num : nums) {
+            int size = finall.size();
+            for (int i = 0; i < size; i++) {
+                List<Integer> subset = new ArrayList<>(finall.get(i));
+                subset.add(num);
+                finall.add(subset);
+            }
+        }
 
-// backtracking
-
-// class Solution {
-// static List<List<Integer>> finall;
-// public List<List<Integer>> subsets(int[] nums) {
-// finall=new ArrayList<>();
-// List<Integer> arr=new ArrayList<>();
-// backtrack(nums,arr,0);
-// return finall;
-// }
-// public static void backtrack(int[] nums,List<Integer> arr,int i)
-// {
-// if(!finall.contains(arr))
-// finall.add(new ArrayList<>(arr));
-
-// if(i>nums.length-1)
-// {
-// return;
-// }
-
-// arr.add(nums[i]);
-// backtrack(nums,arr,i+1);
-// arr.remove(arr.size()-1);
-// backtrack(nums,arr,i+1);
-// }
-// }
+        return finall;
+    }
+}
 
 // backtracking
 
-// public class Solution {
-// public List<List<Integer>> subsets(int[] nums) {
-// List<List<Integer>> finall = new ArrayList<>();
-// dfs(nums, 0, new ArrayList<>(), finall);
-// return finall;
-// }
+class subset3 {
+    static List<List<Integer>> finall;
 
-// private void dfs(int[] nums, int index, List<Integer> path,
-// List<List<Integer>> result) {
-// result.add(new ArrayList<>(path));
+    public List<List<Integer>> subsets(int[] nums) {
+        finall = new ArrayList<>();
+        List<Integer> arr = new ArrayList<>();
+        backtrack(nums, arr, 0);
+        return finall;
+    }
 
-// for (int i = index; i < nums.length; i++) {
-// path.add(nums[i]);
-// dfs(nums, i + 1, path, result);
-// path.remove(path.size() - 1); // Backtrack
-// }
-// }
+    public static void backtrack(int[] nums, List<Integer> arr, int i) {
+        if (!finall.contains(arr))
+            finall.add(new ArrayList<>(arr));
 
-// public static void main(String[] args) {
-// Solution solution = new Solution();
-// int[] nums = {1, 2, 3};
-// List<List<Integer>> result = solution.subsets(nums);
-// System.out.println(result);
-// }
-// }
+        if (i > nums.length - 1) {
+            return;
+        }
+
+        arr.add(nums[i]);
+        backtrack(nums, arr, i + 1);
+        arr.remove(arr.size() - 1);
+        backtrack(nums, arr, i + 1);
+    }
+}
+
+// backtracking
+
+class subset5 {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> finall = new ArrayList<>();
+        dfs(nums, 0, new ArrayList<>(), finall);
+        return finall;
+    }
+
+    private void dfs(int[] nums, int index, List<Integer> path,
+            List<List<Integer>> result) {
+        result.add(new ArrayList<>(path));
+
+        for (int i = index; i < nums.length; i++) {
+            path.add(nums[i]);
+            dfs(nums, i + 1, path, result);
+            path.remove(path.size() - 1); // Backtrack
+        }
+    }
+
+    public static void main(String[] args) {
+        subset5 solution = new subset5();
+        int[] nums = { 1, 2, 3 };
+        List<List<Integer>> result = solution.subsets(nums);
+        System.out.println(result);
+    }
+}
 
 // bits manipulation
 
-// public class Solution {
-// public List<List<Integer>> subsets(int[] nums) {
-// List<List<Integer>> finall = new ArrayList<>();
-// int n = nums.length;
-// int numOfSubsets = 1 << n; // 2^n subsets
+class subset6 {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> finall = new ArrayList<>();
+        int n = nums.length;
+        int numOfSubsets = 1 << n; // 2^n subsets
 
-// for (int i = 0; i < numOfSubsets; i++) {
-// List<Integer> subset = new ArrayList<>();
-// for (int j = 0; j < n; j++) {
-// if ((i & (1 << j)) != 0) {
-// subset.add(nums[j]);
-// }
-// }
-// finall.add(subset);
-// }
+        for (int i = 0; i < numOfSubsets; i++) {
+            List<Integer> subset = new ArrayList<>();
+            for (int j = 0; j < n; j++) {
+                if ((i & (1 << j)) != 0) {
+                    subset.add(nums[j]);
+                }
+            }
+            finall.add(subset);
+        }
 
-// return finall;
-// }
+        return finall;
+    }
 
-// public static void main(String[] args) {
-// Solution solution = new Solution();
-// int[] nums = {1, 2, 3};
-// List<List<Integer>> result = solution.subsets(nums);
-// System.out.println(result);
-// }
-// }
+    public static void main(String[] args) {
+        subset6 solution = new subset6();
+        int[] nums = { 1, 2, 3 };
+        List<List<Integer>> result = solution.subsets(nums);
+        System.out.println(result);
+    }
+}
